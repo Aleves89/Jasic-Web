9bb7fe071648794d50675af0f4f2443e594547b5
Aleves89 <alejandrovescovo@gmail.com>
2026-02-02 12:23:01 -0300

    actualizacion

---
+++ assets/js/analytics.js
@@ -0,0 +1,15 @@
+// Google Analytics
+(function () {
+  var gtagScript = document.createElement("script");
+  gtagScript.async = true;
+  gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-425YKFZRV4";
+  document.head.appendChild(gtagScript);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+
+  gtag("js", new Date());
+  gtag("config", "G-XXXXXXXXXX");
+})();
+
